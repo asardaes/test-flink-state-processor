9f53ee1047283df3ccf3927deb11dec7774c582f
Alexis Sarda-Espinosa <alexis_marino.sarda_espinosa@mercedes-benz.com>
2024-07-09 15:38:47 +0200

    Avoid RocksDB reads

--- src/main/kotlin/com/test/state/ActiveServicesCacheBootstrapper.kt
+++ src/main/kotlin/com/test/state/ActiveServicesCacheBootstrapper.kt
@@ -1,5 +1,6 @@
 package com.test.state
 
+import com.test.input.GenericServiceCompositeKey
 import com.test.input.PartitionKey
 import com.test.input.Statuses
 import org.apache.flink.api.common.state.MapState
@@ -8,18 +9,31 @@ import org.apache.flink.state.api.functions.KeyedStateBootstrapFunction
 
 class ActiveServicesCacheBootstrapper : KeyedStateBootstrapFunction<PartitionKey, Statuses>() {
 
+    private var latestKey: String? = null
+
+    private val helperMap = mutableMapOf<GenericServiceCompositeKey, MutableSet<String>>()
+
     @Transient
-    private lateinit var mapState: MapState<String, MutableSet<String>?>
+    private lateinit var mapState: MapState<GenericServiceCompositeKey, MutableSet<String>?>
 
     override fun open(parameters: Configuration?) {
         super.open(parameters)
         mapState = runtimeContext.getMapState(StateDescriptors.ACTIVE_SERVICES)
     }
 
+    /**
+     * IMPORTANT: state processor API requires batch mode,
+     * and Flink sorts the data by key,
+     * so the logic takes advantage of that.
+     */
     override fun processElement(value: Statuses, ctx: Context) {
+        if (value.key != latestKey) {
+            latestKey = value.key
+            helperMap.values.forEach(MutableSet<String>::clear)
+        }
         value.services.forEach {
-            val key = it.serviceId
-            val set = mapState.get(key) ?: mutableSetOf()
+            val key = GenericServiceCompositeKey(it.serviceId, it.countryCode)
+            val set = helperMap.computeIfAbsent(key) { mutableSetOf() }
             set.add(value.id)
             mapState.put(key, set)
         }
--- src/main/kotlin/com/test/state/StateDescriptors.kt
+++ src/main/kotlin/com/test/state/StateDescriptors.kt
@@ -1,13 +1,15 @@
 package com.test.state
 
+import com.test.input.GenericServiceCompositeKey
 import com.test.serde.SetTypeInfo
 import org.apache.flink.api.common.state.MapStateDescriptor
+import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.common.typeinfo.Types
 
 object StateDescriptors {
-    val ACTIVE_SERVICES: MapStateDescriptor<String, MutableSet<String>> = MapStateDescriptor(
+    val ACTIVE_SERVICES: MapStateDescriptor<GenericServiceCompositeKey, MutableSet<String>> = MapStateDescriptor(
         "ActiveServicesCache",
-        Types.STRING,
+        TypeInformation.of(GenericServiceCompositeKey::class.java),
         SetTypeInfo(Types.STRING),
     )
 }
